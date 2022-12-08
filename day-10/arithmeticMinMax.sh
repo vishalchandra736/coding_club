527aa9544ae1e5667c0d42bebfb1a224d2034775
vishalchandra736 <109501380+vishalchandra736@users.noreply.github.com>
2022-12-08 12:31:58 +0530

    Add files via upload

---
+++ day-10/arithmeticMinMax.sh
@@ -0,0 +1,17 @@
+declare -A dict
+read -p 'enter 1st num ' a
+read -p 'enter 2nd num ' b
+read -p 'enter 3rd num ' c
+dict[res1]=$((a+b*c));
+dict[res2]=$((c+a/b));
+dict[res3]=$((a%b+c));
+dict[res4]=$((a*b+c));
+arr=();
+arr=(${dict[res1]} ${dict[res2]} ${dict[res3]} ${dict[res4]});
+
+readarray -t sorted < <(for a in "${arr[@]}"; do echo "$a"; done | sort -n)
+readarray -t sorted2 < <(for a in "${arr[@]}"; do echo "$a"; done | sort -nr)
+
+echo "Ascending Order : "${sorted[@]};
+echo "Descending Order : "${sorted2[@]};
+echo "Arr = "${arr[@]};
